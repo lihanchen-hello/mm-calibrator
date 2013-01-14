dc7947cc8eb45db6f422980fbe67602f4842eb4a
stephen.vidas@gmail.com <stephen.vidas@gmail.com@fd54f5bc-8e1a-060f-12dd-0a35fe31f754>
2013-01-14 09:42:26 +0000

    Some minor bug fixes.

--- trunk/src/extrinsics.cpp
+++ trunk/src/extrinsics.cpp
@@ -274,7 +274,7 @@ void optimizeCalibrationSets(cv::vector<Size> imSize,
         while (newCorners.at(0).size() < (unsigned int)(num))
         {
 
-            printf("%s << newCorners.at(0).size() = %d; num = %d\n", __FUNCTION__, newCorners.at(0).size(), num);
+            printf("%s << newCorners.at(0).size() = %d; num = %d\n", __FUNCTION__, (int)newCorners.at(0).size(), num);
 
             maxIndex = 0;
             maxScore = 0;
